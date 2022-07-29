aac86337bf72e4b0c1899c8bbfa27ac5dcd90548
Ebraim Oliveira <ebraim_oliveira@hotmail.com>
2022-07-29 17:06:18 -0300

    resolvendo requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,29 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  return entrants.reduce((acc, visitant) => {
+    if (visitant.age < 18) {
+      acc.child += 1;
+    } else if (visitant.age >= 18 && visitant.age < 50) {
+      acc.adult += 1;
+    } else {
+      acc.senior += 1;
+    }
+    return acc;
+  }, { child: 0, adult: 0, senior: 0 });
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.values(entrants).length === 0) {
+    return 0;
+  }
+  return entrants.reduce((acc, visitant) => {
+    if (visitant.age < 18) acc += data.prices.child;
+    else if (visitant.age >= 18 && visitant.age < 50) acc += data.prices.adult;
+    else acc += data.prices.senior;
+
+    return parseFloat(acc.toFixed(2));
+  }, 0);
 }
 
 module.exports = { calculateEntry, countEntrants };
