b019bbc7e8139660c972a5e2585bf19b8985cc72
Ebraim Oliveira <ebraim_oliveira@hotmail.com>
2022-07-29 18:03:08 -0300

    resolvendo requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,29 +1,43 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  return entrants.reduce((acc, visitant) => {
-    if (visitant.age < 18) {
-      acc.child += 1;
-    } else if (visitant.age >= 18 && visitant.age < 50) {
-      acc.adult += 1;
-    } else {
-      acc.senior += 1;
-    }
-    return acc;
-  }, { child: 0, adult: 0, senior: 0 });
-}
+// const entrants = [
+//   { name: 'Lara Carvalho', age: 5 },
+//   { name: 'Frederico Moreira', age: 5 },
+//   { name: 'Pedro Henrique Carvalho', age: 5 },
+//   { name: 'Maria Costa', age: 18 },
+//   { name: 'Núbia Souza', age: 18 },
+//   { name: 'Carlos Nogueira', age: 50 },
+// ];
 
-function calculateEntry(entrants) {
-  if (!entrants || Object.values(entrants).length === 0) {
-    return 0;
+const countEntrants = (entrants) => entrants.reduce((acc, visitant) => {
+  if (visitant.age < 18) {
+    acc.child += 1;
+  } else if (visitant.age >= 18 && visitant.age < 50) {
+    acc.adult += 1;
+  } else {
+    acc.senior += 1;
   }
-  return entrants.reduce((acc, visitant) => {
-    if (visitant.age < 18) acc += data.prices.child;
-    else if (visitant.age >= 18 && visitant.age < 50) acc += data.prices.adult;
-    else acc += data.prices.senior;
+  return acc;
+}, { child: 0, adult: 0, senior: 0 });
 
-    return parseFloat(acc.toFixed(2));
-  }, 0);
-}
+const calculateEntry = (entrants) => {
+  if (!entrants || Object.values(entrants).length === 0) {
+    return 0; //! Object.values(entrants).length
+  }
+  // return entrants.reduce((acc, visitant) => {
+  //   if (visitant.age < 18) acc += data.prices.child;
+  //   else if (visitant.age >= 18 && visitant.age < 50) acc += data.prices.adult;
+  //   else acc += data.prices.senior;
+  //   },0)
+  // return parseFloat(acc.toFixed(2));
+  // return entrants.map((param) => param.age); // [5, 5 ,5 ,18 ,18 , 50]
 
+  const visitantList = Object.values(countEntrants(entrants)); // [3, 2, 1]
+  const sum = (visitantList[0] * data.prices.child)
+    + (visitantList[1] * data.prices.adult)
+    + (visitantList[2] * data.prices.senior);
+  return sum;
+};
+
+// console.log(calculateEntry(entrants));
 module.exports = { calculateEntry, countEntrants };
