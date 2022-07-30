e7f3f97c26b69a23c9deb4a9de65a79c2aa62173
Ebraim Oliveira <ebraim_oliveira@hotmail.com>
2022-07-30 20:08:09 -0300

    resolvido parametro 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
-function isManager(id) {
-  // seu código aqui
-}
-
-function getRelatedEmployees(managerId) {
-  // seu código aqui
+const { employees } = data; // === data.employess (só funciona se a variável for extraida de um objeto).
+// verificar se é gerente.
+// retorna true||false
+const isManager = (id) => {
+  let myManager = false;
+  employees.forEach((employee) => {
+    if (employee.managers.some((manager) => manager === id)) myManager = true;
+  });
+  return myManager;
+};
+console.log(isManager());
+function getRelatedEmployees(id) {
+  // isManager === true || false
+  if (!isManager(id)) throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  return employees.reduce((acc, employee) => {
+    if (employee.managers.some((manager) => id === manager)) {
+      acc.push(`${employee.firstName} ${employee.lastName}`);
+    }
+    return acc;
+  }, []);
 }
 
 module.exports = { isManager, getRelatedEmployees };
