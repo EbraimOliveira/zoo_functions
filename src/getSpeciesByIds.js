9a0b28150d6fc8b182318755a00e2899e5c0e4ac
Ebraim Oliveira <ebraim_oliveira@hotmail.com>
2022-07-29 23:16:16 -0300

    resolvido parametro 3

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,15 +1,15 @@
 const data = require('../data/zoo_data');
 
-// const zooAnimals = data.species;
+const zooAnimals = data.species;
 // console.log(zooAnimals);
 // const animalsId = zooAnimals.map((animal) => animal.id);
 // console.log(animalsId);
 
 function getSpeciesByIds(...ids) {
   // if (!ids) return [];
-  return data.species.filter((animalObject) => ids.find((id) => id === animalObject.id));
+  return zooAnimals.filter((animalObject) => ids.find((id) => id === animalObject.id));
 }
 
-console.log(getSpeciesByIds('e8481c1d-42ea-4610-8e11-1752cfc05a46'));
+// console.log(getSpeciesByIds('e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 module.exports = getSpeciesByIds;
