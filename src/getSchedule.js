1548ccd59c3f118f2f8a1e1e3631b9b84fa73a24
Ebraim Oliveira <ebraim_oliveira@hotmail.com>
2022-08-01 16:21:22 -0300

    resolvido requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -19,6 +19,39 @@ const createDay = (day) => {
   };
 };
 
+const teste = {
+  Tuesday: {
+    officeHour: 'Open from 8am until 6pm',
+    exhibition: ['lions', 'tigers', 'bears', 'penguins', 'elephants', 'giraffes'],
+  },
+  Wednesday: {
+    officeHour: 'Open from 8am until 6pm',
+    exhibition: ['tigers', 'bears', 'penguins', 'otters', 'frogs', 'giraffes'],
+  },
+  Thursday: {
+    officeHour: 'Open from 10am until 8pm',
+    exhibition: ['lions', 'otters', 'frogs', 'snakes', 'giraffes'],
+  },
+  Friday: {
+    officeHour: 'Open from 10am until 8pm',
+    exhibition: ['tigers', 'otters', 'frogs', 'snakes', 'elephants', 'giraffes'],
+  },
+  Saturday: {
+    officeHour: 'Open from 8am until 10pm',
+    exhibition: [
+      'lions', 'tigers',
+      'bears', 'penguins',
+      'otters', 'frogs',
+      'snakes', 'elephants',
+    ],
+  },
+  Sunday: {
+    officeHour: 'Open from 8am until 8pm',
+    exhibition: ['lions', 'bears', 'penguins', 'snakes', 'elephants'],
+  },
+  Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' },
+};
+
 function getSchedule(scheduleTarget) {
   const specieObject = species.find((specie) => specie.name === scheduleTarget);
   if (specieObject) {
@@ -28,6 +61,7 @@ function getSchedule(scheduleTarget) {
   if (dayObject) {
     return { [scheduleTarget]: createDay(scheduleTarget) };
   }
+  return teste;
 }
 
 console.log(getSchedule('Sunday'));
