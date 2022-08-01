d6bf0f20280704bb055d3cc4540185157dbac21f
Ebraim Oliveira <ebraim_oliveira@hotmail.com>
2022-08-01 19:51:12 -0300

    resolvendo requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,32 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { species } = data;
+const { employees } = data;
+
+const speciesList = () => {
+  const responsibleForList = employees.map((employee) => employee.responsibleFor)
+  return responsibleForList
 }
+console.log(speciesList())
+
+const employeeReturnObject = (param) => ({
+  id: [param].id,
+  fullName: `${[param].firstName} ${[param].lastName}`,
+  species: speciesList(),
+  locations: locationsList(),
+})
+
+const getEmployeesCoverage = (param) => {
+  const employeeObject = employees.find((emplyee) => {
+    return emplyee.id === Object.values(param)[0] ||
+      emplyee.firstName === Object.values(param)[0] ||
+      emplyee.lastName === Object.values(param)[0]
+  });
+  if (employeeObject) {
+    return employeeReturnObject;
+  }
+}
+
+console.log(getEmployeesCoverage({ name: 'Spry' }));
 
 module.exports = getEmployeesCoverage;
