e52893e99c157099080e26a7535c1b1bf0e9b7c1
Ebraim Oliveira <ebraim_oliveira@hotmail.com>
2022-08-01 12:45:11 -0300

    resolvendo parametro 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const speciesList = data.species;
+const countAnimals = (animal) => {
+  if (!animal) {
+    const allAnimalList = speciesList.reduce((acc, specie) =>
+      acc = { ...acc, [specie.name]: specie.residents.length }, {});
+    return allAnimalList;
+  }
+  if (Object.keys(animal).length === 1) {
+    const specieObject = speciesList.find((specie) => Object.values(animal)[0] === specie.name);
+    return specieObject.residents.length;
+  }
+
+  const specieObject = speciesList.find((specie) => Object.values(animal)[0] === specie.name);
+  const animalsBySex = specieObject.residents.filter((resident) =>
+    resident.sex === Object.values(animal)[1]);
+  return animalsBySex.length;
+};
+// console.log(countAnimals({ specie: 'giraffes', sex: 'female' }));
 
 module.exports = countAnimals;
