bc5808479b4a7b19ac663e1d96eb745ea19fc47f
Ebraim Oliveira <ebraim_oliveira@hotmail.com>
2022-08-01 16:48:18 -0300

    resolvido requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -19,38 +19,8 @@ const createDay = (day) => {
   };
 };
 
-const teste = {
-  Tuesday: {
-    officeHour: 'Open from 8am until 6pm',
-    exhibition: ['lions', 'tigers', 'bears', 'penguins', 'elephants', 'giraffes'],
-  },
-  Wednesday: {
-    officeHour: 'Open from 8am until 6pm',
-    exhibition: ['tigers', 'bears', 'penguins', 'otters', 'frogs', 'giraffes'],
-  },
-  Thursday: {
-    officeHour: 'Open from 10am until 8pm',
-    exhibition: ['lions', 'otters', 'frogs', 'snakes', 'giraffes'],
-  },
-  Friday: {
-    officeHour: 'Open from 10am until 8pm',
-    exhibition: ['tigers', 'otters', 'frogs', 'snakes', 'elephants', 'giraffes'],
-  },
-  Saturday: {
-    officeHour: 'Open from 8am until 10pm',
-    exhibition: [
-      'lions', 'tigers',
-      'bears', 'penguins',
-      'otters', 'frogs',
-      'snakes', 'elephants',
-    ],
-  },
-  Sunday: {
-    officeHour: 'Open from 8am until 8pm',
-    exhibition: ['lions', 'bears', 'penguins', 'snakes', 'elephants'],
-  },
-  Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' },
-};
+const schedule = () =>
+  Object.keys(hours).reduce((acc, day) => ({ ...acc, [day]: createDay(day) }), {});
 
 function getSchedule(scheduleTarget) {
   const specieObject = species.find((specie) => specie.name === scheduleTarget);
@@ -61,7 +31,7 @@ function getSchedule(scheduleTarget) {
   if (dayObject) {
     return { [scheduleTarget]: createDay(scheduleTarget) };
   }
-  return teste;
+  return schedule();
 }
 
 console.log(getSchedule('Sunday'));
