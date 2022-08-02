06b8183b59e7bace0f297b6f997a94e00cbcbaf7
Ebraim Oliveira <ebraim_oliveira@hotmail.com>
2022-08-01 23:24:42 -0300

    resolvendo requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -3,29 +3,32 @@ const data = require('../data/zoo_data');
 const { species } = data;
 const { employees } = data;
 
-const speciesList = () => {
-  const responsibleForList = employees.map((employee) => employee.responsibleFor)
-  return responsibleForList
-}
-console.log(speciesList())
+const speciesList = (param) => {
+  const responsibleForList = param.responsibleFor;
+  const bixoFiltrado = species.filter((specieName) => responsibleForList.includes(specieName.id));
+  return bixoFiltrado.map((bixo) => bixo.name);
+};
 
+const locationsList = (param) => {
+  const responsibleForList = param.responsibleFor;
+  const bixoFiltrado = species.filter((specieName) => responsibleForList.includes(specieName.id));
+  return bixoFiltrado.map((bixo) => bixo.location);
+};
 const employeeReturnObject = (param) => ({
-  id: [param].id,
-  fullName: `${[param].firstName} ${[param].lastName}`,
-  species: speciesList(),
-  locations: locationsList(),
-})
+  id: param.id,
+  fullName: `${param.firstName} ${param.lastName}`,
+  species: speciesList(param),
+  locations: locationsList(param),
+});
 
 const getEmployeesCoverage = (param) => {
-  const employeeObject = employees.find((emplyee) => {
-    return emplyee.id === Object.values(param)[0] ||
-      emplyee.firstName === Object.values(param)[0] ||
-      emplyee.lastName === Object.values(param)[0]
-  });
+  const employeeObject = employees.find((emplyee) => emplyee.id === Object.values(param)[0]
+    || emplyee.firstName === Object.values(param)[0]
+    || emplyee.lastName === Object.values(param)[0]);
   if (employeeObject) {
-    return employeeReturnObject;
+    return employeeReturnObject(employeeObject);
   }
-}
+};
 
 console.log(getEmployeesCoverage({ name: 'Spry' }));
 
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -4,34 +4,34 @@ const { species } = data;
 const { hours } = data;
 
 const getAnimalList = (day) => {
-    const animalObjectList = species.filter((specie) => specie.availability.includes(day));
-    const animalList = animalObjectList.map((object) => object.name);
-    return animalList;
+  const animalObjectList = species.filter((specie) => specie.availability.includes(day));
+  const animalList = animalObjectList.map((object) => object.name);
+  return animalList;
 };
 
 const createDay = (day) => {
-    if (day === 'Monday') {
-        return { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
-    }
-    return {
-        officeHour: `Open from ${hours[day].open}am until ${hours[day].close}pm`,
-        exhibition: getAnimalList(day),
-    };
+  if (day === 'Monday') {
+    return { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
+  }
+  return {
+    officeHour: `Open from ${hours[day].open}am until ${hours[day].close}pm`,
+    exhibition: getAnimalList(day),
+  };
 };
 
 const schedule = () =>
-    Object.keys(hours).reduce((acc, day) => ({ ...acc, [day]: createDay(day) }), {});
+  Object.keys(hours).reduce((acc, day) => ({ ...acc, [day]: createDay(day) }), {});
 
 function getSchedule(scheduleTarget) {
-    const specieObject = species.find((specie) => specie.name === scheduleTarget);
-    if (specieObject) {
-        return specieObject.availability;
-    }
-    const dayObject = Object.keys(hours).find((day) => day === scheduleTarget);
-    if (dayObject) {
-        return { [scheduleTarget]: createDay(scheduleTarget) };
-    }
-    return schedule();
+  const specieObject = species.find((specie) => specie.name === scheduleTarget);
+  if (specieObject) {
+    return specieObject.availability;
+  }
+  const dayObject = Object.keys(hours).find((day) => day === scheduleTarget);
+  if (dayObject) {
+    return { [scheduleTarget]: createDay(scheduleTarget) };
+  }
+  return schedule();
 }
 
 console.log(getSchedule('Sunday'));
